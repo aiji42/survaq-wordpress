7c57bd2e660d367346e68f040e01af4b991ee664
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-10-06 00:32:45 +0900

    update Docker image and entrypoint

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse --dir-mode=755 --file-mode=644 --implicit-dirs -o allow_other $BUCKET $MNT_DIR
+gcsfuse --debug_gcs --debug_fuse --dir-mode=755 --file-mode=644 --implicit-dirs --experimental-local-file-cache -o allow_other $BUCKET $MNT_DIR
 echo "Mounting completed."
 
 # Apacheをバックグラウンドで実行
