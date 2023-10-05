9a4eaeb3d04c505eac0bddc98c17002601c7f8c0
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-10-06 01:31:26 +0900

    update Docker image and entrypoint

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-GCS_BUCKET="your-gcs-bucket-name"
 LOCAL_DIR="/var/www/html/wp-content"
 
 mkdir -p $LOCAL_DIR/themes $LOCAL_DIR/plugins $LOCAL_DIR/upgrade
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $GCS_BUCKET/wp-content/themes $LOCAL_DIR/themes
-gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $GCS_BUCKET/wp-content/plugins $LOCAL_DIR/plugins
-gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $GCS_BUCKET/wp-content/upgrade $LOCAL_DIR/upgrade
+gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $BUCKET/wp-content/themes $LOCAL_DIR/themes
+gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $BUCKET/wp-content/plugins $LOCAL_DIR/plugins
+gcsfuse --debug_gcs --debug_fuse --implicit-dirs --experimental-local-file-cache -o allow_other $BUCKET/wp-content/upgrade $LOCAL_DIR/upgrade
 echo "Mounting completed."
 
 # Apacheをバックグラウンドで実行
